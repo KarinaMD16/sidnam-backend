77607c2d843516d311c470029d466faa163a5987
heyitskarinamd@outlook.com <heyitskarinamd@outlook.com>
2025-07-04 22:14:29 -0600

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: 'http://localhost:5173/',
-  credentials: true, // solo si usas cookies o auth headers
+  origin: 'http://localhost:5173',
+  
 });
 
   await app.listen(process.env.PORT ?? 3000);
