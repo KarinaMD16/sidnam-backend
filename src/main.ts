d9b065afec40c1cbeaed5fe205e41a959f82c1f7
JOSE NU�EZ VILLALOBOS <jose.nunez.villalobos@est.una.ac.cr>
2025-07-13 21:06:54 -0600

    pipe

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
 
 });
 
+  app.useGlobalPipes(new ValidationPipe()); 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
