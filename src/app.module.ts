6b82288a77d6d23a72193fd86123ce8528e12208
elein06 <elein06@example.com>
2025-07-31 07:39:03 -0600

    remover modulo voluntarios no usado y usar configuracion de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,28 +17,40 @@ import { VoluntariadoModule } from './modulos/voluntariado/voluntariado.module';
 import { Contacto_emergencia } from './modulos/voluntariado/entities/contactoEmergencia.entity';
 import { Horario } from './modulos/voluntariado/entities/horario.entity';
 import { SolicitudAprobada } from './modulos/voluntariado/entities/solicitudAprobada.entity';
-import { SolicitudPendiente } from './modulos/voluntariado/entities/solicitudPendiente.entity';
-import { Tipo_voluntariado } from './modulos/voluntariado/entities/tipoVoluntariado.entity';
-import { Voluntario } from './modulos/voluntariado/entities/voluntariado.entity';
-import { ContactoEmergenciaPendiente } from './modulos/voluntariado/entities/contactoEmergenciaPendiente';
-import { HorarioPendiente } from './modulos/voluntariado/entities/horarioPendiente.entity';
+import { SolicitudPendiente } from "./modulos/voluntariado/entities/solicitudPendiente.entity";
+import { ContactoEmergenciaPendiente } from "./modulos/voluntariado/entities/contactoEmergenciaPendiente";
+import { HorarioPendiente } from "./modulos/voluntariado/entities/horarioPendiente.entity";
 
-
+import * as dotenv from "dotenv";
+dotenv.config();
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, 
+      isGlobal: true,
     }),
     PublicacionesModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Rafaeli06.',
-      database: 'SIDNAMDB',
-      entities: [Donacion, Eventos, Proyectos, Galeria, Categoria, Usuario, Contacto_emergencia, ContactoEmergenciaPendiente, Horario, HorarioPendiente, SolicitudAprobada, SolicitudPendiente],
-      synchronize: false,
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [
+        Donacion,
+        Eventos,
+        Proyectos,
+        Galeria,
+        Categoria,
+        Usuario,
+        Contacto_emergencia,
+        ContactoEmergenciaPendiente,
+        Horario,
+        HorarioPendiente,
+        SolicitudAprobada,
+        SolicitudPendiente,
+      ],
+      synchronize: true,
       dropSchema: true,
     }),
     GaleriaModule,
