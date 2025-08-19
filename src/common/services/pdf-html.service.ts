0a3966a18360d5e6ee5e8e8168fcba153d312a60
Axxxxxel11 <128259347+Axxxxxel11@users.noreply.github.com>
2025-08-18 20:30:09 -0600

    reporte donaciones por mes

--- src/common/services/pdf-html.service.ts
+++ src/common/services/pdf-html.service.ts
@@ -28,3 +28,28 @@ export class PdfHtmlService {
     res.send(pdfBuffer);
   }
 }
+
+@Injectable()
+export class PdfHtmlDonacionService {
+  async generarDesdeHtmlConNombre(html: string, res: Response, filename: string) {
+    const browser = await puppeteer.launch({ headless: true });
+    try {
+      const page = await browser.newPage();
+      await page.setContent(html, { waitUntil: 'networkidle0', timeout: 30000 }); // o 'networkidle0', 'domcontentloaded'
+      const pdfBuffer = await page.pdf({
+        format: 'A4',
+        printBackground: true,
+        margin: { top: '40px', bottom: '40px', left: '30px', right: '30px' },
+      });
+
+      res.set({
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': `attachment; filename="${filename}"`,
+        'Content-Length': pdfBuffer.length,
+      });
+      res.send(pdfBuffer);
+    } finally {
+      await browser.close();
+    }
+  }
+}
--- src/modulos/solicitud-donacion/solicitudDonacion.module.ts
+++ src/modulos/solicitud-donacion/solicitudDonacion.module.ts
@@ -14,7 +14,7 @@ import { CreateRegistroDonacionUseCase } from "./use-cases/registro/create-Regis
 import { GetRegistrosDonacionUseCase } from "./use-cases/registro/get-RegistroDonacion.use-case";
 import { UpdateRegistroDonacionUseCase } from "./use-cases/registro/update-RegistroDonacion.use-case";
 import { ReporteDonacionesService } from "./reporteDonacion.service";
-import { PdfHtmlService } from "src/common/services/pdf-html.service";
+import { PdfHtmlDonacionService, PdfHtmlService } from "src/common/services/pdf-html.service";
 
 
 
@@ -24,7 +24,7 @@ import { PdfHtmlService } from "src/common/services/pdf-html.service";
         AutenticacionModule,
         GestionUsuarioModule,
     ],
-    providers: [SolicitudDonacionService, SolicitudDonacionGateway, CreateSolicitudDonacionUseCase, GetSolicitudesDonacionUseCase, CreateRegistroDonacionUseCase, GetRegistrosDonacionUseCase, UpdateRegistroDonacionUseCase, ReporteDonacionesService, PdfHtmlService],
+    providers: [SolicitudDonacionService, SolicitudDonacionGateway, CreateSolicitudDonacionUseCase, GetSolicitudesDonacionUseCase, CreateRegistroDonacionUseCase, GetRegistrosDonacionUseCase, UpdateRegistroDonacionUseCase, ReporteDonacionesService, PdfHtmlService, PdfHtmlDonacionService],
     controllers: [SolicitudDonacionController],
     exports: [SolicitudDonacionService],
 })
