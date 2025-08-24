7a3d640aeda2066326ac57cda7e3784190215a55
JOSE NU�EZ VILLALOBOS <jose.nunez.villalobos@est.una.ac.cr>
2025-08-23 19:02:55 -0600

    faltante a la hora de actualizar

--- src/common/enums/dependencia.enum.ts
+++ src/common/enums/dependencia.enum.ts
@@ -7,3 +7,8 @@ export const DependenciaOpts = [
   { id: 1, value: Dependencia.Dependiente, nombre: 'Dependiente' },
   { id: 2, value: Dependencia.Independiente, nombre: 'Independiente' },
 ];
+
+export const getDependenciaById = (id: number): Dependencia | null => {
+  const option = DependenciaOpts.find(opt => opt.id === id);
+  return option ? option.value : null;
+};
