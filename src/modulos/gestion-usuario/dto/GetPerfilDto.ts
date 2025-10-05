8271eb8a5c6cd5459a807d3d3e88ddb3c5895521
JOSE NU�EZ VILLALOBOS <jose.nunez.villalobos@est.una.ac.cr>
2025-10-04 18:10:15 -0600

    get perfil

---
+++ src/modulos/gestion-usuario/dto/GetPerfilDto.ts
@@ -0,0 +1,20 @@
+import { Expose } from "class-transformer";
+
+export class PerfilUsuario{
+
+    @Expose()
+    id: number;
+
+    @Expose()
+    cedula: string;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    apellido1: string;
+
+    @Expose()
+    apellido2: string;
+
+}
