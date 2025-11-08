2612fb5a96aa7f4a2f67063be253872bb06112f9
JOSE NU�EZ VILLALOBOS <jose.nunez.villalobos@est.una.ac.cr>
2025-11-07 19:30:53 -0600

    a ver

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,31 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-  origin: [
-    'http://localhost:5173', 
-    'https://hogar-san-blas-informativa.vercel.app', 
-    'https://sidnam-administrativa.vercel.app'
-  ],
-  credentials: true,
-});
+  app.use(cookieParser());
 
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://hogar-san-blas-informativa.vercel.app',
+      'https://sidnam-administrativa.vercel.app',
+    ],
+    credentials: true, 
+  });
 
   app.useGlobalPipes(new ValidationPipe());
-  
+
   const config = new DocumentBuilder()
     .setTitle('API SIDNAM')
     .setDescription('Documentación de la API del sistema de voluntariado')
     .setVersion('1.0')
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); 
+  SwaggerModule.setup('api', app, document);
 
-  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
 
+bootstrap();
